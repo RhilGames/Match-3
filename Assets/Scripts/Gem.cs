547c93e4546f61c258019e1a88932af1484e6676
Christopher Singleton <csingleton@rhilgames.com>
2022-11-23 15:45:12 -0800

    Swiping Gems

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -9,6 +9,12 @@ public class Gem : MonoBehaviour
     [HideInInspector]
     public Board board;
 
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+
+    private bool mousePressed;
+    private float swipeAngle = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +24,30 @@ public class Gem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(mousePressed && Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+
+            finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateAngle();
+        }
     }
 
     public void SetUpGem(Vector2Int pos, Board theBoard)
     {
         posIndex = pos;
         board = theBoard;
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePressed = true;
+    }
+
+    private void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x);
+        swipeAngle = swipeAngle * 180 / Mathf.PI;
     }
 }
